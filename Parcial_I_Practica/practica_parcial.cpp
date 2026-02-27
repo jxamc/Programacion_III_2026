c671d03fc2163a2b170662b71c4d406cd2881a40
jxamc <jxamc@miumg.edu.gt>
2026-02-27 13:40:55 -0600

    Update practica_parcial.cpp

--- Parcial_I_Practica/practica_parcial.cpp
+++ Parcial_I_Practica/practica_parcial.cpp
@@ -8,17 +8,10 @@ struct Nodo {
 
 int main() {
 
-    // Declaración de punteros
-    Nodo* primero = nullptr;
-    Nodo* segundo = nullptr;
-    Nodo* tercero = nullptr;
+    Nodo* primero = new Nodo();
+    Nodo* segundo = new Nodo();
+    Nodo* tercero = new Nodo();
 
-    // Creación dinámica de nodos
-    primero = new Nodo();
-    segundo = new Nodo();
-    tercero = new Nodo();
-
-    // Solicitar datos al usuario
     cout << "Ingrese el valor del primer nodo: ";
     cin >> primero->dato;
 
@@ -28,19 +21,16 @@ int main() {
     cout << "Ingrese el valor del tercer nodo: ";
     cin >> tercero->dato;
 
-    // Enlace de nodos
     primero->siguiente = segundo;
     segundo->siguiente = tercero;
     tercero->siguiente = nullptr;
 
-    // Mostrar información del estudiante
-    cout << "\nUniversidad Mariano Gálvez" << endl;
+    cout << "\nUniversidad Mariano Galvez" << endl;
     cout << "Curso: Programacion III" << endl;
     cout << "Nombre del estudiante: Julio Cesar Xam Chivalan" << endl;
-    cout << "Carné: 9941-24-8897" << endl;
+    cout << "Carne: 9941-24-8897" << endl;
     cout << "Seccion: B" << endl;
 
-    // Mostrar valores almacenados accediendo mediante punteros
     cout << "\nValores almacenados en los nodos:" << endl;
 
     Nodo* auxiliar = primero;
@@ -50,7 +40,6 @@ int main() {
         auxiliar = auxiliar->siguiente;
     }
 
-    // Liberación correcta de memoria
     delete primero;
     delete segundo;
     delete tercero;
